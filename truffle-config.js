c5381844bf490eb7806a3529298e941bb99312f4
Anderson H. da Rosa <andersonhrosa@outlook.com>
2022-10-13 18:20:30 -0300

    1. First Commit

---
+++ truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "172.25.176.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
